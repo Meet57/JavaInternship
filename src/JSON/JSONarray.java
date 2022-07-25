8cac9bfbb79658957467b58ee63226e18a5574d0
meet57 <meet57.mp@gmail.com>
2022-07-25 10:18:39 +0530

    Enum

--- src/JSON/JSONarray.java
+++ src/JSON/JSONarray.java
@@ -1,49 +1,30 @@
 package JSON;
 
-import jdk.nashorn.internal.ir.debug.JSONWriter;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import javax.xml.bind.SchemaOutputResolver;
 import java.io.*;
 import java.util.ArrayList;
 
-@SuppressWarnings("ALL")
-public class readJSON {
+public class JSONarray {
     public static void main(String[] args) throws IOException {
-//        File file = new File("/home/meet/IdeaProjects/Practice/out/production/Practice/JSON/x");
-//        Scanner sc = new Scanner(file);
-//
-//        while (sc.hasNextLine())
-//            System.out.println(sc.nextLine());
-
         File file = new File("/home/meet/IdeaProjects/Practice/out/production/Practice/JSON/x");
         BufferedReader bf = new BufferedReader(new FileReader(file));
-        String st,data[],date="";
+        String st,data[];
         JSONObject main = new JSONObject(),temp;
         ArrayList<JSONObject> day = new JSONArray();
 
         while ((st = bf.readLine()) != null){
             data = st.split(" ");
-            if(!date.equals(data[0])){
-                if(day.size() == 0){
-                    date = data[0];
-                    continue;
-                }
-                main.put(date.replace("/","-"),day);
-                date = data[0];
-                day.clear();
-            }
-//            temp.clear();
             temp = new JSONObject();
-
+            temp.put("date",data[0].replace('/',' '));
             temp.put("time",data[1]);
             temp.put("cpu",data[2]);
             temp.put("mem",data[3]);
-            temp.put("total",data[4]);
+            temp.put("total mem",data[4]);
             day.add(temp);
-         }
-        main.put(date.replace("/","-"),day);
+        }
+        main.put("data",day);
 
         FileWriter writer = new FileWriter("/home/meet/IdeaProjects/Practice/src/JSON/final.json");
         BufferedWriter bw = new BufferedWriter(writer);
---
+++ src/Strings/Test.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Test {
+    public static void main(String[] args) {
+        String s1 = new String("Meet");
+        String s2 = new String("Meet");
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+    }
+}
